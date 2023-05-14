07b2a87c70fb768371d4d41d262eaa2078300f79
MarcosHVB <marcos.boas@sptech.school>
2023-05-14 20:02:49 -0300

    upando jar completo

---
+++ project-easy/src/main/java/sptech/jswing/models/Conexao2.java
@@ -0,0 +1,22 @@
+package sptech.jswing.models;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class Conexao2 {
+    private JdbcTemplate connection;
+
+    
+    public Conexao2() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://3.82.226.130:3306/banco1?useSSL=false");
+        dataSource.setUsername("root");
+        dataSource.setPassword("urubu100");
+        this.connection = new JdbcTemplate(dataSource);
+    }
+    
+    public JdbcTemplate getConnection() {
+        return connection;
+    }
+}
